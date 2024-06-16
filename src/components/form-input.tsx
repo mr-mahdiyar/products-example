d814cdcbe382764de191968800448576c2a7e909
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-16 17:17:15 +0330

    some styles and value handling process were added.

--- src/components/form-input.tsx
+++ src/components/form-input.tsx
@@ -1,14 +1,34 @@
+import { Dispatch, SetStateAction } from "react";
+
 type FormInputProps = {
   lable: string;
   name: string;
-  type: string
+  type: string;
+  value: string | number;
+  setValue: Dispatch<SetStateAction<string>>;
 };
-export default function FormInput({lable, name, type}: FormInputProps) {
+export default function FormInput({
+  lable,
+  name,
+  type,
+  value,
+  setValue,
+}: FormInputProps) {
+  function changeHandler (event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value)
+  }
   return (
     <div className="mt-2">
       <label htmlFor={name}>{lable}</label>
       <br />
-      <input id={name} type={type} name={name} className="border border-black mt-1 rounded-md w-full "/>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        className="border border-black mt-1 rounded-md w-full p-1"
+        value={value}
+        onChange={changeHandler}
+      />
     </div>
   );
 }
